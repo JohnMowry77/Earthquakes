e47e466d7b217b95ba2ef2051b0200f9a4d59f1f
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-04-16 16:19:21 -0600

    update API_KEY

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,7 +13,8 @@ var myMap= L.map('mapid').setView([40.0150, -105.2705], 13)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v10',
+    // id: 'mapbox/light-v10',
+    id: 'mapbox/outdoors-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -24,6 +25,8 @@ d3.json(base_url).then(function(data) {
     console.log(data);
 
 });
+
+
 
 
 
